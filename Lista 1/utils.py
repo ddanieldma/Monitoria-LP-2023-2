1423482e37afa6c7ffee32c3cf50f9db113c9563
Daniel de Miranda Almeida <B51995@fgv.edu.br>
2023-09-11 09:06:39 -0300

    ate questao 3

--- Lista 1/utils.py
+++ Lista 1/utils.py
@@ -5,9 +5,9 @@ import data
 dataset = pd.DataFrame(data.people_data)
 
 #================Functions=============
-# Questão
+# Questão 1
 def add_person(data: pd.DataFrame, name: str, age: int, city: str, hobbies: list) -> pd.DataFrame:
-	"""Adds person to the dataframe
+	"""Add person to the dataframe
 
 	Keyword arguments:
 	data -- the dataframe to add the person to
@@ -41,7 +41,53 @@ def add_person(data: pd.DataFrame, name: str, age: int, city: str, hobbies: list
 
 	return data
 
+# Questão 2
+def remove_person(data: pd.DataFrame, name: str) -> pd.DataFrame:
+	"""Remove person from the dataframe
+
+	:param pd.DataFrame data: Datagrame to modify
+    :param str num2: Name of person to delete
+    :rtype: pd.DataFrame
+    :return: The data frame modified
+	"""
+	if not data[data["name"] == name].empty:
+		# se o nome não está presente na coluna names
+		
+		# remove pelo índice do nome que bate  ocm a variável
+		data.drop(data[data["name"] == name].index, inplace = True)
+
+	return data
+
+# questao 3
+def get_ages(data: pd.DataFrame) -> tuple:
+	"""Get ages max, min and mean
+
+	:param pd.DataFrame data: Dataframe with the ages
+	:rtype: tuple
+	:return: Tuple with max min and mean of ages, in this order 
+	"""
 	
+	ages = data["age"]
+	max_age = ages.max()
+	min_age = ages.min()
+	mean_age = int(ages.mean())
+
+	return (max_age, min_age, mean_age)
+
+# questao 4
+def get_hobbies(data: pd.DataFrame) -> set:
+	"""Get the set of hobbies of dataframe
+
+	:param pd.DataFrame data: Dataframe with hobbies
+	:rtype: set
+	:return: Set with all the hobbies in the dataframe
+	"""
+	# get the list of hobbies
+	hobbies_list = data["hobbies"].to_list()
+	
+	# and return as set
+	return set(hobbies_list)
+
 
 #============Tests====================
 if __name__ == "__main__":
@@ -54,5 +100,10 @@ if __name__ == "__main__":
 	# print(hobbies_list)
 
 	print(dataset)
+	print(get_ages(dataset))
 	add_person(dataset, "Daniel", 18, "Belo Horizonte", ["music", "guitar"])
 	print(dataset)
+	print(get_ages(dataset))
+	remove_person(dataset, "Daniel")
+	print(dataset)
+	print(get_ages(dataset))
